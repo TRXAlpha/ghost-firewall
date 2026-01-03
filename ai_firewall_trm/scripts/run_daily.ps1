7ea4190aa211ad969fb6592d5c6e492b5bec7428
ChrisLUVNoSkill <77664017+TRXAlpha@users.noreply.github.com>
2026-01-03 18:10:26 +0200

    Delete ai_firewall_trm directory

--- ai_firewall_trm/scripts/run_daily.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [Parameter(Mandatory = $true)][string]$LogPath,
-    [Parameter(Mandatory = $true)][string]$ConfigPath,
-    [Parameter(Mandatory = $true)][string]$OutJson,
-    [string]$OutText
-)
-
-python "$PSScriptRoot\..\src\ai_firewall\cli.py" --log $LogPath --config $ConfigPath --out $OutJson --text $OutText
